d66a386bb32ffe5168c75f5116f95e4148625bc1
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-15 11:03:56 +0300

    Задание 2-4 - добавлена функция isPalindrome()

--- PythonLab1Task2-4/PythonLab1Task2_4.py
+++ PythonLab1Task2-4/PythonLab1Task2_4.py
@@ -14,6 +14,23 @@ class Task2:
         
         result = ''.join(ls)
         print('Перемешанная строка: ' + result)
-
-
+    
+    # Дана строка, состоящая из символов латиницы. Необходимо проверить, образуют ли прописные символы этой строки палиндром.
+    def IsPalindrome():
+        print('Введите строку: ')
+        stroka = input()
+        
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        upper_case_letters = ''
+        
+        for i in range(0, len(stroka)):
+            if (alphabet.find(stroka[i]) != -1):
+                upper_case_letters += stroka[i]
+                
+        length_upp_cs_ltr = len(upper_case_letters)
+        for i in range(0, length_upp_cs_ltr // 2 + length_upp_cs_ltr % 2):
+            if (upper_case_letters[i] != upper_case_letters[length_upp_cs_ltr - 1 - i]):
+                return False
+            
+        return True
     
