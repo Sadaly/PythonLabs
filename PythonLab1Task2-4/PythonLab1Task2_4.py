7fbecc219d9de71609068648819e0f4c54b15394
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-15 11:47:02 +0300

    Задание 2-4 - добавлена функция SortWordsInString(str) - добавлена возможность выбора задания - Функции ShuffleString() и IsPalindrome() теперь принимаю аргумент типа str, вместо ввода значения при вызове функции - Из функции ShuffleString() и IsPalindrome() удалены команды вывода в консоль

--- PythonLab1Task2-4/PythonLab1Task2_4.py
+++ PythonLab1Task2-4/PythonLab1Task2_4.py
@@ -1,25 +1,19 @@
 # -*- coding: cp1251 -*-
 import random
+from re import I, split
 
 class Task2: 
     
     # Дана строка. Необходимо перемешать все символы строки в случайном порядке.
-    def ShuffleString():
-        print('Введите строку: ')
-        stroka = input()
-        print('Изначальная строка: ' + stroka)
-        
+    def ShuffleString(stroka):
         ls = list(stroka)
         random.shuffle(ls)
         
         result = ''.join(ls)
-        print('Перемешанная строка: ' + result)
+        return result
     
     # Дана строка, состоящая из символов латиницы. Необходимо проверить, образуют ли прописные символы этой строки палиндром.
-    def IsPalindrome():
-        print('Введите строку: ')
-        stroka = input()
-        
+    def IsPalindrome(stroka):
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
         upper_case_letters = ''
         
@@ -34,3 +28,42 @@ class Task2:
             
         return True
     
+    # Дана строка в которой записаны слова через пробел. Необходимо упорядочить слова по количеству букв в каждом слове.
+    def SortWordsInString(stroka):
+        words = stroka.split()
+        words_count = len(words)
+        result = ''
+
+        for i in range(0, words_count - 1):
+            for j in range(i, words_count):
+                if (len(words[i]) > len(words[j])):
+                    temp = words[i]
+                    words[i] = words[j]
+                    words[j] = temp
+        
+        for i in range(0, words_count):
+            result += words[i] + ' '
+        
+        return result
+    
+    while(True):
+        print('1.ShuffleString(str)\n2.IsPalindrome(str)\n3.SortWordsInString(str)\nВведите номер необходимой функции:')
+        task_number = int(input())
+        
+        print('Введите строку: ')
+        stroka = input()
+        
+        if(task_number == 1):
+            print('Перемешанная строка: ' + ShuffleString(stroka))
+        elif(task_number == 2):
+            print('Прописаные буквы строки образуют палиндром?: ' + str(IsPalindrome(stroka)))
+        elif(task_number == 3):
+            print('Отсортированная строка по длине слов: ' + SortWordsInString(stroka))
+            
+        print()
+        
+            
+        
+
+            
+        
