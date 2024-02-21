b4b1a26e4b9cd2683053a6c852492740c25bfdc1
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-21 12:15:36 +0300

    Лабараторная 1 Задание 12 (В порядке увеличения медианного значения выборки строк (прошлое медианное значение удаляется из выборки и производится поиск нового медианного значения.) -реализовано в функции SortByMedianAsciiDifference(strings)

--- PythonLab1Task11-14/PythonLab1Task11-14/PythonLab1Task11_14.py
+++ PythonLab1Task11-14/PythonLab1Task11-14/PythonLab1Task11_14.py
@@ -3,3 +3,10 @@
 def SortByAvgAsciiWeight(strings):
     return sorted(strings, key=lambda x: sum(map(ord, x))/len(x))
 
+def SortByMedianAsciiDifference(strings):
+    def median_difference(s):
+        sorted_chars = sorted(map(ord, s))
+        median_index = len(sorted_chars) // 2
+        return abs(sorted_chars[median_index] - sorted_chars[median_index-1])
+
+    return sorted(strings, key=median_difference)
