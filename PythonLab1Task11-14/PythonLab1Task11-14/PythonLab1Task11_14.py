0ded4b465e0b53854b3984cc4ac29fa96ced600b
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-21 12:18:49 +0300

    Лабараторная 1 Задание 13 (В порядке увеличения квадратичного отклонения между наибольшим ASCII-кодом символа строки и разницы в ASCII-кодах пар зеркально расположенных символов строки (относительно ее середины)) -реализовано в функции SortByQuadDeviation(strings)

--- PythonLab1Task11-14/PythonLab1Task11-14/PythonLab1Task11_14.py
+++ PythonLab1Task11-14/PythonLab1Task11-14/PythonLab1Task11_14.py
@@ -10,3 +10,12 @@ def SortByMedianAsciiDifference(strings):
         return abs(sorted_chars[median_index] - sorted_chars[median_index-1])
 
     return sorted(strings, key=median_difference)
+
+def SortByQuadDeviation(strings):
+    def quad_deviation(s):
+        middle = len(s) // 2
+        mirror_pairs = zip(s[:middle], reversed(s[middle:]))
+        return sum((ord(a) - ord(b))**2 for a, b in mirror_pairs)
+
+    return sorted(strings, key=quad_deviation)
+
