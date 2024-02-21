28a9153077879b3af0e32d5f9d8bb1c8b2ab4c5e
Sadaly <mm12112004@mail.ru>
2024-02-21 23:08:13 +0300

    Лабораторная 3 (Rectangle Quad is_intersect, compare) -изменен класс Quad:     -теперь является наследником Rectangle     -добавлено условие проверки:         -равны ли смежные грани, а именно верхняя и левая -изменен класс Rectangle:     -небольшие изменения в представлении объектов     -добавлено условие проверки:         -равны ли параллельные стороны, верхняя и нижняя, левая и правая         -равны ли диагонали         -если хотя бы 2 из 4-ех точек совпадают, то вылетает ошибка -добавлен пример для ручного тестирования

--- Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -8,13 +8,39 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
     
-def segment_length(point1, point2):
+def segment_length(point1, point2): # Длина отрезка по двум точкам
     return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5   
 
 class Rectangle:
-    def __init__(self, top_left, top_right, bottom_left, bottom_right):
+    str_name = "Прямоугольник"
+    
+
+    def __init__(self, top_left, top_right, bottom_right, bottom_left):
+        # Проверяем совпадают ли точки
+        def are_points_equal(point1, point2, point3, point4):
+            if (point1.x == point2.x and point1.y == point2.y):
+                return True
+            elif (point1.x == point3.x and point1.y == point3.y):
+                return True
+            elif (point1.x == point4.x and point1.y == point4.y):
+                return True
+            elif (point2.x == point3.x and point2.y == point3.y):
+                return True
+            elif (point2.x == point4.x and point2.y == point4.y):
+                return True
+            elif (point3.x == point4.x and point3.y == point4.y):
+                return True
+
+            return False
+        
+        # Проверяем равны ли параллельные стороны и диагонали
+        if (are_points_equal(top_left, top_right, bottom_right, bottom_left) or (segment_length(top_left, bottom_right) != segment_length(top_right, bottom_left)) or (segment_length(top_left, top_right) != segment_length(bottom_left, bottom_right)) or (segment_length(top_left, bottom_left) != segment_length(top_right, bottom_right))):
+            raise ValueError(f"Фигура не является {self.str_name}ом")
+            
+                
         self.top_left = top_left
         self.top_right = top_right
+        
         self.bottom_left = bottom_left
         self.bottom_right = bottom_right
 
@@ -24,32 +50,25 @@ class Rectangle:
         return width * height
 
     def __str__(self):
-        return f"Прямоугольник (A: {self.top_left}, B: {self.top_right}, C: {self.bottom_left}, D: {self.bottom_right})"
+        return f"{self.str_name} (A: {self.top_left}, B: {self.top_right}, C: {self.bottom_left}, D: {self.bottom_right})"
     
-class Quad:
-    def __init__(self, top_left, top_right, bottom_left, bottom_right):
-        self.top_left = top_left
-        self.top_right = top_right
-        self.bottom_left = bottom_left
-        self.bottom_right = bottom_right
-
-    def area(self):
-        width = segment_length(self.top_left, self.top_right)
-        return width * width
-
-    def __str__(self):
-        return f"Квадрат (A: {self.top_left}, B: {self.top_right}, C: {self.bottom_left}, D: {self.bottom_right})"
+class Quad(Rectangle):
+    str_name = "Квадрат"
+    def __init__(self, top_left, top_right, bottom_right, bottom_left):
+        if (segment_length(top_left, top_right) != segment_length(top_left, bottom_left)):
+            raise ValueError(f"Фигура не является {self.str_name}ом")
+        super().__init__(top_left, top_right, bottom_right, bottom_left)
 
 def compare(t1, t2):
     area1 = t1.area()
     area2 = t2.area()
     
     if (area1 > area2):
-        print(f'{t1} больше {t2}')
+        return (f'{t1} больше {t2}')
     elif (area1 == area2):
-        print(f'{t1} равен {t2}')
+        return (f'{t1} равен {t2}')
     elif (area1 < area2):
-        print(f'{t1} меньше {t2}')
+        return (f'{t1} меньше {t2}')
         
 def is_intersect(t1, t2):
     def find_intersection(point1, point2, point3, point4): # Проверяем пересекаются ли 2 отрезка
@@ -85,3 +104,11 @@ def is_intersect(t1, t2):
     
     return intersection1 or intersection2 or intersection3 or intersection4 
 
+# Пример
+rec1 = Rectangle(Point(0, 0), Point(0, 3), Point(3, 3), Point(3, 0))
+rec2 = Quad(Point(-1, -1), Point(-1, -4), Point(-4, -4), Point(-4, -1))
+
+print(compare(rec1, rec2))
+print(is_intersect(rec1, rec2))
+
+rec3 = Quad(Point(0, 0), Point(1, 1), Point(0, 0), Point(1, 1))
