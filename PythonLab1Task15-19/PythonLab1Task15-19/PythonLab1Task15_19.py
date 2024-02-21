071f2d6cf69cb548b41e02c1c5f973ada47ccf9a
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-21 13:35:56 +0300

    Лабараторная 1 Задание 19 (Дан список. Построить новый список из квадратов неотрицательных чисел, меньших 100 и встречающихся в массиве больше 2 раз.) -реализовано в функции SquaresGreaterThan2(arr) -Добавлена возможность выбора функции

--- PythonLab1Task15-19/PythonLab1Task15-19/PythonLab1Task15_19.py
+++ PythonLab1Task15-19/PythonLab1Task15-19/PythonLab1Task15_19.py
@@ -29,3 +29,47 @@ def FindPositiveDivisors(nums):
                 unique_divisors.add(i)
 
     return list(unique_divisors)
+
+def SquaresGreaterThan2(arr):
+    unique_squares = set()
+    result = []
+
+    for num in arr:
+        square = num**2
+        if num >= 0 and square < 100 and arr.count(num) > 2 and square not in unique_squares:
+            result.append(square)
+            unique_squares.add(square)
+
+    return result
+
+# Пользователь выбирает задачу
+task_choice = int(input("Выберите функцию (1, 2, 3, 4, or 5):\n1. Дан целочисленный массив, в котором лишь один элемент отличается  от остальных. Необходимо найти значение этого элемента.\n2. Дан целочисленный массив. Необходимо найти два наименьших элемента.\n3. Дано вещественное число R и массив вещественных чисел. Найти элемент массива, который наиболее близок к данному числу.\n4. Для введенного списка положительных чисел построить список всех положительных делителей элементов списка без повторений.\n5. Дан список. Построить новый список из квадратов неотрицательных чисел, меньших 100 и встречающихся в массиве больше 2 раз.\n"))
+
+# В зависимости от выбора пользователя, выполняем соответствующую задачу
+if task_choice == 1:
+    arr = list(map(int, input("Введите массив чисел (элементы разделены пробелами): ").split()))
+    result = FindUniqueElement(arr)
+    print(f"Уникальный элемент: {result}")
+
+elif task_choice == 2:
+    arr = list(map(int, input("Введите массив чисел (элементы разделены пробелами): ").split()))
+    result = FindTwoMinElements(arr)
+    print(f"Два наименьших элемента: {result}")
+
+elif task_choice == 3:
+    R = float(input("Введите вещественное число R: "))
+    arr = list(map(float, input("Введите массив вещественных чисел (элементы разделены пробелами): ").split()))
+    result = FindClosestElement(R, arr)
+    print(f"Ближайший элемент к {R} это {result}")
+
+elif task_choice == 4:
+    nums = list(map(int, input("Введите массив положительных чисел (элементы разделены пробелами): ").split()))
+    result = FindPositiveDivisors(nums)
+    print(f"Положительные делители: {result}")
+
+elif task_choice == 5:
+    arr = list(map(int, input("Введите массив чисел (элементы разделены пробелами): ").split()))
+    result = SquaresGreaterThan2(arr)
+    print(f"Квадраты элементов повторяющихся больше 2 раз элементов и меньше 100: {result}")
+
+
