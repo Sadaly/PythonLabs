c41f67e627064dce7a7b41986165e03de142a436
Sadaly <mm12112004@mail.ru>
2024-02-21 21:29:54 +0300

    Лабораторная 3 (Rectangle Quad is_intersect, compare) -добавлен метод segment_length -обновлён класс Rectangle:     -изменен метод area     -исправлено строковое представление -добавлен метод is_intersect (проверяет пересекаются ли грани четырехугольников):     -метод find_intersection теперь подметод     -добавлен метод is_segment_intersect_figure

--- Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -8,35 +8,58 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
     
+def segment_length(point1, point2):
+    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5   
+
 class Rectangle:
     def __init__(self, top_left, top_right, bottom_left, bottom_right):
         self.top_left = top_left
         self.top_right = top_right
         self.bottom_left = bottom_left
         self.bottom_right = bottom_right
-        
 
     def area(self):
-        width = self.top_right.x - self.bottom_left.x
-        height = self.top_right.y - self.bottom_left.y
+        width = segment_length(self.top_left, self.top_right)
+        height = segment_length(self.top_left, self.bottom_left)
         return width * height
 
     def __str__(self):
-        return f"Rectangle (bottom left: {self.bottom_left}, top right: {self.top_right})"
+        return f"Rectangle (top left: {self.top_left}, top right: {self.top_right}), (bottom left: {self.bottom_left}, bottom right: {self.bottom_right})"
 
 
-def find_intersection(point1, point2, point3, point4):
-    # ѕроверка на параллельность
-    if (point2.x - point1.x) * (point4.y - point3.y) == (point4.x - point3.x) * (point2.y - point1.y):
-        return None  # ќтрезки параллельны, нет пересечени€
+
     
-    t1 = ((point4.x - point3.x) * (point1.y - point3.y) - (point4.y - point3.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
-    t2 = ((point2.x - point1.x) * (point1.y - point3.y) - (point2.y - point1.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
+def is_intersect(t1, t2):
+    def find_intersection(point1, point2, point3, point4): # ѕровер€ем пересекаютс€ ли 2 отрезка
+        # ѕроверка на параллельность
+        if (point2.x - point1.x) * (point4.y - point3.y) == (point4.x - point3.x) * (point2.y - point1.y):
+            return False  # ќтрезки параллельны, нет пересечени€
+        
+        t1 = ((point4.x - point3.x) * (point1.y - point3.y) - (point4.y - point3.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
+        t2 = ((point2.x - point1.x) * (point1.y - point3.y) - (point2.y - point1.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
 
-    # ѕроверка наличи€ точки пересечени€ внутри отрезков
-    if 0 <= t1 <= 1 and 0 <= t2 <= 1:
-        intersection_x = point1.x + t1 * (point2.x - point1.x)
-        intersection_y = point1.y + t1 * (point2.y - point1.y)
-        return intersection_x, intersection_y
-    else:
-        return None  # “очка пересечени€ не лежит внутри отрезков
+        # ѕроверка наличи€ точки пересечени€ внутри отрезков
+        if 0 <= t1 <= 1 and 0 <= t2 <= 1:
+            intersection_x = point1.x + t1 * (point2.x - point1.x)
+            intersection_y = point1.y + t1 * (point2.y - point1.y)
+            return True
+        else:
+            return False  # “очка пересечени€ не лежит внутри отрезков
+        
+    def is_segment_intersect_figure(point1, point2, t1): # ѕровер€ем пересекает ли отрезок фигуру
+        intersection1 = find_intersection(point1, point2, t1.top_left, t1.top_right)
+        intersection2 = find_intersection(point1, point2, t1.top_right, t1.bottom_right)
+        
+        intersection3 = find_intersection(point1, point2, t1.bottom_right, t1.bottom_left)
+        intersection4 = find_intersection(point1, point2, t1.bottom_left, t1.top_left)
+           
+        return intersection1 or intersection2 or intersection3 or intersection4
+    
+    intersection1 = is_segment_intersect_figure(t1.top_left, t1.top_right, t2)
+    intersection2 = is_segment_intersect_figure(t1.top_right, t1.bottom_right, t2)
+    
+    intersection3 = is_segment_intersect_figure(t1.bottom_right, t1.bottom_left, t2)
+    intersection4 = is_segment_intersect_figure(t1.bottom_left, t1.top_left, t2)
+    
+    return intersection1 or intersection2 or intersection3 or intersection4 
+
