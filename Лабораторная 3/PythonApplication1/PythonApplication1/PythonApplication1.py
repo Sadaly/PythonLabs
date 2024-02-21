4df606eef81c6eca84d28da2aafd25c825cad20f
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-02-21 20:19:49 +0300

    Лабораторная 3 (Rectangle Quad is_intersect, compare) -добавлен класс Point -изменен метод find_intersection под класс Point поиска координат точки пересечения 2-ух отрезков -добавлен класс Rectangle

--- Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Лабораторная 3/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,17 +1,42 @@
 # -*- coding:cp1251 -*-
 
-def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    
+class Rectangle:
+    def __init__(self, top_left, top_right, bottom_left, bottom_right):
+        self.top_left = top_left
+        self.top_right = top_right
+        self.bottom_left = bottom_left
+        self.bottom_right = bottom_right
+        
+
+    def area(self):
+        width = self.top_right.x - self.bottom_left.x
+        height = self.top_right.y - self.bottom_left.y
+        return width * height
+
+    def __str__(self):
+        return f"Rectangle (bottom left: {self.bottom_left}, top right: {self.top_right})"
+
+
+def find_intersection(point1, point2, point3, point4):
     # Проверка на параллельность
-    if (x2 - x1) * (y4 - y3) == (x4 - x3) * (y2 - y1):
+    if (point2.x - point1.x) * (point4.y - point3.y) == (point4.x - point3.x) * (point2.y - point1.y):
         return None  # Отрезки параллельны, нет пересечения
     
-    t1 = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
-    t2 = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+    t1 = ((point4.x - point3.x) * (point1.y - point3.y) - (point4.y - point3.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
+    t2 = ((point2.x - point1.x) * (point1.y - point3.y) - (point2.y - point1.y) * (point1.x - point3.x)) / ((point4.y - point3.y) * (point2.x - point1.x) - (point4.x - point3.x) * (point2.y - point1.y))
 
     # Проверка наличия точки пересечения внутри отрезков
     if 0 <= t1 <= 1 and 0 <= t2 <= 1:
-        intersection_x = x1 + t1 * (x2 - x1)
-        intersection_y = y1 + t1 * (y2 - y1)
+        intersection_x = point1.x + t1 * (point2.x - point1.x)
+        intersection_y = point1.y + t1 * (point2.y - point1.y)
         return intersection_x, intersection_y
     else:
         return None  # Точка пересечения не лежит внутри отрезков
